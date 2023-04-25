57fbd6a8b5620278ffc5857bab3484adae06e535
AntTal13 <anthonytalarico13@gmail.com>
2023-04-25 13:51:28 -0400

    admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Stone, Use
+from .models import Stone, Use, Villain
 
 # Register your models here
 admin.site.register(Stone)
 admin.site.register(Use)
+admin.site.register(Villain)
